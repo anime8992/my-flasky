00588dcaca19016fb9973a9f44f32524ad3ec8e1
Kevin <jianzhuo.song@tbiedu.com>
2018-01-15 18:06:01 +0800

    update hello.py

--- hello.py
+++ hello.py
@@ -2,12 +2,21 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment 
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
 
 
 @app.errorhandler(404)
@@ -22,8 +31,7 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
