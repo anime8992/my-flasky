0b34889940a86740f964a03bff4a96cd0daef756
Kevin <jianzhuo.song@tbiedu.com>
2018-01-15 10:44:31 +0800

    update

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template
+from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+from flask_moment import Moment 
 
 app = Flask(__name__)
-
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -28,3 +29,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    manager.run()
